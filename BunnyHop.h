f838c66f13543e2795ccba5208497e9514750acc
Hitbokx <kaanchan.tiwary@gmail.com>
2023-04-14 15:30:02 +0530

    Modified.

--- BunnyHop.h
+++ BunnyHop.h
@@ -3,21 +3,6 @@
 #include "Structs.h"
 #include <Windows.h>
 
-/*inline void BunnyHop( PlayerEnt* pLocalPlayer, uintptr_t cModuleBase )
-{
-	if ( GetAsyncKeyState( VK_SPACE ) )
-	{
-		if ( pLocalPlayer->flag & FLAG.onGround )
-		{
-			*(uintptr_t*)(cModuleBase + offs.fJump) |= force.jump;
-		}
-		else
-		{
-			*(uintptr_t*)(cModuleBase + offs.fJump) &= ~force.jump;
-		}
-	}
-}*/
-
 bool isPlayerMoving( PlayerEnt* pLocalPlayer )
 {
 	int playerVel{ (int)(pLocalPlayer->velocity.x + pLocalPlayer->velocity.y + pLocalPlayer->velocity.z) };
